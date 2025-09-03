884a6ff330d21af575defbc80b2e8d5380c431b4
ellie <umellie8@gmail.com>
2025-09-04 00:12:57 +0330

    fix errors in user input

--- ping_time.py
+++ ping_time.py
@@ -8,25 +8,28 @@ def ping_timer(func):
             result = func(*args, **kwargs)
         finally:
             end_time = time.time()
-            print(f"\nRequest completed in {end_time - start_time *1000:.2f:} ms")
+            ping_time = end_time - start_time
+            print(f"\nRequest completed in {ping_time:.2f} OR {ping_time *1000:.2f} ms")
         return result
     return wrapper
 
+
+@ping_timer
 def ping_request():
     user_input = input("Enter a website address:").strip()
 
-    if not user_input.startswith("http://", "https://"):
+    if not user_input.startswith(("http://", "https://")):
         user_input = "https://" + user_input
 
     try:
         response = requests.get(user_input, timeout=5)
         print(f"Status code is : {response.status_code}")
-        print(f"Website URL is : {response.url}")
+        print(f"Website exact URL is : {response.url}")
         print(f"Content size is : {len(response.content)} bytes")
         return True
     except requests.exceptions.RequestException as e:
         print(f"Error : Couldn't reach the website! ({e})")
-        return None, user_input
+        return user_input
     
 if __name__ == "__main__":
     ping_request()
